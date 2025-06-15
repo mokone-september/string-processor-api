1cb36da5170c5103bd914e909be63a2a4f04b27d
Thabiso <48449108+mokone-september@users.noreply.github.com>
2025-06-15 09:22:55 +0200

    Delete dist directory

--- dist/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorHandler(err, _req, res, _next) {
-    console.error('Unhandled Error:', err.stack);
-    res.status(500).json({ error: 'Internal Server Error', details: err.message });
-}
-exports.default = errorHandler;
